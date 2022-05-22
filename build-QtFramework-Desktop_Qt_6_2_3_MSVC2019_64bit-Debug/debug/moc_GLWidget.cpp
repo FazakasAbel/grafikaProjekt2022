45bedba6729d0459e50d56855a680a902e7b51d0
FazakasAbel <100289883+FazakasAbel@users.noreply.github.com>
2022-05-22 17:17:59 +0300

    Add files via upload

---
+++ build-QtFramework-Desktop_Qt_6_2_3_MSVC2019_64bit-Debug/debug/moc_GLWidget.cpp
@@ -0,0 +1,344 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'GLWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../QtFramework/GUI/GLWidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'GLWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_cagd__GLWidget_t {
+    const uint offsetsAndSize[80];
+    char stringdata0[657];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_cagd__GLWidget_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_cagd__GLWidget_t qt_meta_stringdata_cagd__GLWidget = {
+    {
+QT_MOC_LITERAL(0, 14), // "cagd::GLWidget"
+QT_MOC_LITERAL(15, 12), // "set_x_signal"
+QT_MOC_LITERAL(28, 0), // ""
+QT_MOC_LITERAL(29, 12), // "set_y_signal"
+QT_MOC_LITERAL(42, 12), // "set_z_signal"
+QT_MOC_LITERAL(55, 20), // "set_knot_point_ratio"
+QT_MOC_LITERAL(76, 7), // "animate"
+QT_MOC_LITERAL(84, 11), // "set_angle_x"
+QT_MOC_LITERAL(96, 5), // "value"
+QT_MOC_LITERAL(102, 11), // "set_angle_y"
+QT_MOC_LITERAL(114, 11), // "set_angle_z"
+QT_MOC_LITERAL(126, 15), // "set_zoom_factor"
+QT_MOC_LITERAL(142, 11), // "set_trans_x"
+QT_MOC_LITERAL(154, 11), // "set_trans_y"
+QT_MOC_LITERAL(166, 11), // "set_trans_z"
+QT_MOC_LITERAL(178, 19), // "set_show_second_der"
+QT_MOC_LITERAL(198, 18), // "set_show_first_der"
+QT_MOC_LITERAL(217, 19), // "set_div_point_count"
+QT_MOC_LITERAL(237, 18), // "set_selected_curve"
+QT_MOC_LITERAL(256, 9), // "set_scale"
+QT_MOC_LITERAL(266, 19), // "animateCyclicMotion"
+QT_MOC_LITERAL(286, 25), // "set_selected_cyclic_curve"
+QT_MOC_LITERAL(312, 18), // "set_selected_point"
+QT_MOC_LITERAL(331, 23), // "update_selected_point_x"
+QT_MOC_LITERAL(355, 23), // "update_selected_point_y"
+QT_MOC_LITERAL(379, 23), // "update_selected_point_z"
+QT_MOC_LITERAL(403, 13), // "setCyclicDraw"
+QT_MOC_LITERAL(417, 15), // "move_knot_point"
+QT_MOC_LITERAL(433, 23), // "set_selected_knot_point"
+QT_MOC_LITERAL(457, 10), // "saveCurves"
+QT_MOC_LITERAL(468, 20), // "animateSurfaceMotion"
+QT_MOC_LITERAL(489, 20), // "set_selected_surface"
+QT_MOC_LITERAL(510, 16), // "set_texture_draw"
+QT_MOC_LITERAL(527, 19), // "set_selected_shader"
+QT_MOC_LITERAL(547, 14), // "set_selected_r"
+QT_MOC_LITERAL(562, 14), // "set_selected_g"
+QT_MOC_LITERAL(577, 14), // "set_selected_b"
+QT_MOC_LITERAL(592, 20), // "set_selected_scaling"
+QT_MOC_LITERAL(613, 22), // "set_selected_smoothing"
+QT_MOC_LITERAL(636, 20) // "set_selected_shading"
+
+    },
+    "cagd::GLWidget\0set_x_signal\0\0set_y_signal\0"
+    "set_z_signal\0set_knot_point_ratio\0"
+    "animate\0set_angle_x\0value\0set_angle_y\0"
+    "set_angle_z\0set_zoom_factor\0set_trans_x\0"
+    "set_trans_y\0set_trans_z\0set_show_second_der\0"
+    "set_show_first_der\0set_div_point_count\0"
+    "set_selected_curve\0set_scale\0"
+    "animateCyclicMotion\0set_selected_cyclic_curve\0"
+    "set_selected_point\0update_selected_point_x\0"
+    "update_selected_point_y\0update_selected_point_z\0"
+    "setCyclicDraw\0move_knot_point\0"
+    "set_selected_knot_point\0saveCurves\0"
+    "animateSurfaceMotion\0set_selected_surface\0"
+    "set_texture_draw\0set_selected_shader\0"
+    "set_selected_r\0set_selected_g\0"
+    "set_selected_b\0set_selected_scaling\0"
+    "set_selected_smoothing\0set_selected_shading"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_cagd__GLWidget[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      37,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       4,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,  236,    2, 0x06,    1 /* Public */,
+       3,    1,  239,    2, 0x06,    3 /* Public */,
+       4,    1,  242,    2, 0x06,    5 /* Public */,
+       5,    1,  245,    2, 0x06,    7 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       6,    0,  248,    2, 0x0a,    9 /* Public */,
+       7,    1,  249,    2, 0x0a,   10 /* Public */,
+       9,    1,  252,    2, 0x0a,   12 /* Public */,
+      10,    1,  255,    2, 0x0a,   14 /* Public */,
+      11,    1,  258,    2, 0x0a,   16 /* Public */,
+      12,    1,  261,    2, 0x0a,   18 /* Public */,
+      13,    1,  264,    2, 0x0a,   20 /* Public */,
+      14,    1,  267,    2, 0x0a,   22 /* Public */,
+      15,    1,  270,    2, 0x0a,   24 /* Public */,
+      16,    1,  273,    2, 0x0a,   26 /* Public */,
+      17,    1,  276,    2, 0x0a,   28 /* Public */,
+      18,    1,  279,    2, 0x0a,   30 /* Public */,
+      19,    1,  282,    2, 0x0a,   32 /* Public */,
+      20,    0,  285,    2, 0x0a,   34 /* Public */,
+      21,    1,  286,    2, 0x0a,   35 /* Public */,
+      22,    1,  289,    2, 0x0a,   37 /* Public */,
+      23,    1,  292,    2, 0x0a,   39 /* Public */,
+      24,    1,  295,    2, 0x0a,   41 /* Public */,
+      25,    1,  298,    2, 0x0a,   43 /* Public */,
+      26,    1,  301,    2, 0x0a,   45 /* Public */,
+      27,    1,  304,    2, 0x0a,   47 /* Public */,
+      28,    1,  307,    2, 0x0a,   49 /* Public */,
+      29,    0,  310,    2, 0x0a,   51 /* Public */,
+      30,    0,  311,    2, 0x0a,   52 /* Public */,
+      31,    1,  312,    2, 0x0a,   53 /* Public */,
+      32,    1,  315,    2, 0x0a,   55 /* Public */,
+      33,    1,  318,    2, 0x0a,   57 /* Public */,
+      34,    1,  321,    2, 0x0a,   59 /* Public */,
+      35,    1,  324,    2, 0x0a,   61 /* Public */,
+      36,    1,  327,    2, 0x0a,   63 /* Public */,
+      37,    1,  330,    2, 0x0a,   65 /* Public */,
+      38,    1,  333,    2, 0x0a,   67 /* Public */,
+      39,    1,  336,    2, 0x0a,   69 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void, QMetaType::Double,    8,
+    QMetaType::Void, QMetaType::Double,    8,
+    QMetaType::Void, QMetaType::Double,    8,
+    QMetaType::Void, QMetaType::Double,    8,
+    QMetaType::Void, QMetaType::Bool,    8,
+    QMetaType::Void, QMetaType::Bool,    8,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+
+       0        // eod
+};
+
+void cagd::GLWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<GLWidget *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->set_x_signal((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 1: _t->set_y_signal((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 2: _t->set_z_signal((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 3: _t->set_knot_point_ratio((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 4: _t->animate(); break;
+        case 5: _t->set_angle_x((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->set_angle_y((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: _t->set_angle_z((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->set_zoom_factor((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 9: _t->set_trans_x((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 10: _t->set_trans_y((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 11: _t->set_trans_z((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 12: _t->set_show_second_der((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 13: _t->set_show_first_der((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 14: _t->set_div_point_count((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->set_selected_curve((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: _t->set_scale((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 17: _t->animateCyclicMotion(); break;
+        case 18: _t->set_selected_cyclic_curve((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 19: _t->set_selected_point((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 20: _t->update_selected_point_x((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 21: _t->update_selected_point_y((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 22: _t->update_selected_point_z((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 23: _t->setCyclicDraw((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 24: _t->move_knot_point((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 25: _t->set_selected_knot_point((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 26: _t->saveCurves(); break;
+        case 27: _t->animateSurfaceMotion(); break;
+        case 28: _t->set_selected_surface((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 29: _t->set_texture_draw((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 30: _t->set_selected_shader((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 31: _t->set_selected_r((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 32: _t->set_selected_g((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 33: _t->set_selected_b((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 34: _t->set_selected_scaling((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 35: _t->set_selected_smoothing((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 36: _t->set_selected_shading((*reinterpret_cast< double(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (GLWidget::*)(double );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&GLWidget::set_x_signal)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (GLWidget::*)(double );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&GLWidget::set_y_signal)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (GLWidget::*)(double );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&GLWidget::set_z_signal)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (GLWidget::*)(double );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&GLWidget::set_knot_point_ratio)) {
+                *result = 3;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject cagd::GLWidget::staticMetaObject = { {
+    QMetaObject::SuperData::link<QOpenGLWidget::staticMetaObject>(),
+    qt_meta_stringdata_cagd__GLWidget.offsetsAndSize,
+    qt_meta_data_cagd__GLWidget,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_cagd__GLWidget_t
+, QtPrivate::TypeAndForceComplete<GLWidget, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<bool, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<bool, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<bool, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<bool, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<double, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *cagd::GLWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *cagd::GLWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_cagd__GLWidget.stringdata0))
+        return static_cast<void*>(this);
+    return QOpenGLWidget::qt_metacast(_clname);
+}
+
+int cagd::GLWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QOpenGLWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 37)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 37;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 37)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 37;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void cagd::GLWidget::set_x_signal(double _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void cagd::GLWidget::set_y_signal(double _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void cagd::GLWidget::set_z_signal(double _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 2, _a);
+}
+
+// SIGNAL 3
+void cagd::GLWidget::set_knot_point_ratio(double _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 3, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
